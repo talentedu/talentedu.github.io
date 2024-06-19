d557bc575db828674c5306bde947ef50adcc3abf
talentedu <isabella.lara@talenteducation.com.br>
2024-06-19 16:57:45 -0300

    Initial commit

---
+++ Teste treinamento/assets/playbar/playbarScript.js
@@ -0,0 +1,52 @@
+cp.playbarAssetArr = 
+[
+	'AudioOff',
+	'AudioOn',
+	'BackGround',
+	'Backward',
+	'Color',
+	'ColorSmall',
+	'CC',
+	'Exit',
+	'FastForward',
+	'FastForward1',
+	'FastForward2',
+	'Forward',
+	'Glow',
+	'GlowSmall',
+	'Height',
+	'InnerStroke',
+	'InnerStrokeSmall',
+	'Play',
+	'Pause',
+	'Progress',
+	'Rewind',
+	'Shade',
+	'ShadeSmall',
+	'Stroke',
+	'StrokeSmall',
+	'Thumb',
+	'ThumbBase',
+	'TOC'
+];
+cp.playbarTooltips = 
+{
+	AudioOff : "Áudio desligado ",
+	AudioOn : "Áudio ligado ",
+	Backward : "Voltar ",
+	CC : "Legenda codificada ",
+	Exit : "Sair ",
+	FastForward : "Velocidade 2x de avançar ",
+	FastForward1 : "Velocidade 4x de avançar ",
+	FastForward2 : "Velocidade normal ",
+	Forward : "Avançar ",
+	Play : "Reproduzir ",
+	Pause : "Pausar ",
+	Rewind : "Retroceder " ,
+	TOC : "Índice analítico ",
+	Info : "Informações ",
+	Print : "Imprimir "
+};
+cp.handleSpecialForPlaybar = function(playbarConstruct)
+{
+}
